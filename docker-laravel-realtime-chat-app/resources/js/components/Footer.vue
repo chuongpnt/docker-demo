bb442eeeb37b366aeb6e35d23f7daa6fbf7ee3cd
KuSamoa <chuong@whl.travel>
2020-04-28 20:06:18 +0700

    docker demo

---
+++ docker-laravel-realtime-chat-app/resources/js/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer id="footer" class="w-100">
+    <div>
+        Broadcast through Presence Channel for Shared Room and Private Channel for private message
+    </div>
+</footer>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+footer {
+  background: #1c5986;
+  color: white;
+  text-align: center;
+  position: absolute;
+  bottom: 0;
+}
+
+@media only screen and (max-width: 768px) {
+  footer {
+    position: initial;
+  }
+}
+
+footer a {
+  color: #00ffe9;
+}
+</style>
---
+++ docker-python/app.py
@@ -0,0 +1,12 @@
+#doc của flask Externally Visible Server. them 0.0.0.0 neu o moi truong localhost
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('index.html', title='Docker Python', name='James')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=os.environ['PORT']) # Chạy project ở PORT nhận vào từ biến môi trường
